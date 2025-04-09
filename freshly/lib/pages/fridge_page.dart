41fe90ada89469710bbbcc48417ba501877f43e5
Varich Maleevan <varich.mal@student.mahidol.edu>
2025-04-10 01:37:01 +0700

    Add fridge page, fix BottomNavBar and AppBar

--- freshly/lib/pages/fridge_page.dart
+++ freshly/lib/pages/fridge_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Fridge extends StatelessWidget {
-  const Fridge({super.key});
+class FridgePage extends StatelessWidget {
+  const FridgePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Text("Fridge Page");
   }
 }
