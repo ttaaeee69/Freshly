2e4d795e552543f23b3cd42ee0f3096ab787eec8
Varich Maleevan <varich.mal@student.mahidol.edu>
2025-04-13 02:45:31 +0700

    add add ingredient page appbar

--- freshly/lib/pages/add_ingredient.dart
+++ freshly/lib/pages/add_ingredient.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class AddIngredientPage extends StatelessWidget {
   const AddIngredientPage({super.key});
@@ -7,11 +8,38 @@ class AddIngredientPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
+        toolbarHeight: 100,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
         ),
-        title: const Text("Add Ingredient"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: HexColor("#2C4340"),
+          onPressed: () => Navigator.pop(context),
+          padding: const EdgeInsets.only(left: 20),
+        ),
+        centerTitle: false,
+        title: Text(
+          "Adding food into your fridge",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.circle_notifications_rounded,
+              size: 40,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+            padding: const EdgeInsets.only(right: 20),
+          ),
+        ],
       ),
     );
   }
