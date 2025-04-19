f10fefc336d399e00b2f3794189a95a60fe0fef8
Varich Maleevan <varich.mal@student.mahidol.edu>
2025-04-19 21:08:16 +0700

    update

--- freshly/lib/pages/fridge/add_cooked.dart
+++ freshly/lib/pages/fridge/add_cooked.dart
@@ -71,246 +71,249 @@ class _AddCookedPageState extends State<AddCookedPage> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          spacing: 30,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: HexColor("#E4C1C1"),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            spacing: 30,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: HexColor("#E4C1C1"),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 20,
+                    children: [
+                      Text(
+                        "Add food's profile",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        spacing: 10,
+                        children: [
+                          IconButton(
+                            icon: Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                color: HexColor("#DEDEDE"),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.add_photo_alternate_outlined,
+                                size: 30,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                          Text(
+                            "or",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                color: HexColor("#DEDEDE"),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.add_reaction_outlined,
+                                size: 30,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              child: Padding(
+              Container(
                 padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: HexColor("#97A78D"),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 20,
+                  spacing: 10,
                   children: [
                     Text(
-                      "Add food's profile",
+                      "Details",
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      spacing: 10,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        IconButton(
-                          icon: Container(
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                              color: HexColor("#DEDEDE"),
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              Icons.add_photo_alternate_outlined,
-                              size: 30,
-                            ),
-                          ),
-                          onPressed: () {},
-                        ),
                         Text(
-                          "or",
+                          "Name :",
                           style: TextStyle(
-                            fontSize: 24,
+                            fontSize: 16,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        IconButton(
-                          icon: Container(
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                              color: HexColor("#DEDEDE"),
-                              shape: BoxShape.circle,
+                        const SizedBox(height: 6),
+                        TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            filled: true,
+                            fillColor: HexColor("#EEF1DA"),
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 16.0,
                             ),
-                            child: Icon(
-                              Icons.add_reaction_outlined,
-                              size: 30,
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                              borderSide: BorderSide.none,
                             ),
                           ),
-                          onPressed: () {},
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: HexColor("#2C4340"),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Since :",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        GestureDetector(
+                          onTap: () async {
+                            DateTime? selectedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                            );
+                            if (selectedDate != null) {
+                              setState(() => startDate = selectedDate);
+                            }
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 16.0,
+                            ),
+                            decoration: BoxDecoration(
+                              color: HexColor("#EEF1DA"),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  startDate != null
+                                      ? DateFormat("dd/MM/yyyy")
+                                          .format(startDate!)
+                                      : "",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: HexColor("#2C4340"),
+                                  ),
+                                ),
+                                Icon(Icons.calendar_month),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Expiration Date :",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        GestureDetector(
+                          onTap: () async {
+                            DateTime? selectedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                            );
+                            if (selectedDate != null) {
+                              setState(() => expirationDate = selectedDate);
+                            }
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 16.0,
+                            ),
+                            decoration: BoxDecoration(
+                              color: HexColor("#EEF1DA"),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  expirationDate != null
+                                      ? DateFormat("dd/MM/yyyy")
+                                          .format(expirationDate!)
+                                      : "",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: HexColor("#2C4340"),
+                                  ),
+                                ),
+                                Icon(Icons.calendar_month),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ],
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: HexColor("#97A78D"),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: HexColor("#ADB2D4"),
+                ),
+                onPressed: () {
+                  _addCooked(context);
+                },
+                child: Text(
+                  "Done",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: HexColor("#2C4340"),
+                  ),
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 10,
-                children: [
-                  Text(
-                    "Details",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Name :",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 6),
-                      TextField(
-                        controller: _nameController,
-                        decoration: InputDecoration(
-                          isDense: true,
-                          filled: true,
-                          fillColor: HexColor("#EEF1DA"),
-                          contentPadding: const EdgeInsets.symmetric(
-                            vertical: 8.0,
-                            horizontal: 16.0,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: HexColor("#2C4340"),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Since :",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 6),
-                      GestureDetector(
-                        onTap: () async {
-                          DateTime? selectedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2000),
-                            lastDate: DateTime(2100),
-                          );
-                          if (selectedDate != null) {
-                            setState(() => startDate = selectedDate);
-                          }
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 8.0,
-                            horizontal: 16.0,
-                          ),
-                          decoration: BoxDecoration(
-                            color: HexColor("#EEF1DA"),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                startDate != null
-                                    ? DateFormat("dd/MM/yyyy")
-                                        .format(startDate!)
-                                    : "",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: HexColor("#2C4340"),
-                                ),
-                              ),
-                              Icon(Icons.calendar_month),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Expiration Date :",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 6),
-                      GestureDetector(
-                        onTap: () async {
-                          DateTime? selectedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2000),
-                            lastDate: DateTime(2100),
-                          );
-                          if (selectedDate != null) {
-                            setState(() => expirationDate = selectedDate);
-                          }
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 8.0,
-                            horizontal: 16.0,
-                          ),
-                          decoration: BoxDecoration(
-                            color: HexColor("#EEF1DA"),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                expirationDate != null
-                                    ? DateFormat("dd/MM/yyyy")
-                                        .format(expirationDate!)
-                                    : "",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: HexColor("#2C4340"),
-                                ),
-                              ),
-                              Icon(Icons.calendar_month),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: HexColor("#ADB2D4"),
-              ),
-              onPressed: () {
-                _addCooked(context);
-              },
-              child: Text(
-                "Done",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: HexColor("#2C4340"),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
