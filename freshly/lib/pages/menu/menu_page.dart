5535b5bcde3adc952dea7cf65d17e6fb82e931ae
Varich Maleevan <varich.mal@student.mahidol.edu>
2025-04-20 20:43:24 +0700

    enable fetch recipe

--- freshly/lib/pages/menu/menu_page.dart
+++ freshly/lib/pages/menu/menu_page.dart
@@ -16,6 +16,7 @@ class _MenuPageState extends State<MenuPage> {
       "54b2b8be87124363836179e73a645e7d"; // Replace with your API key
   Dio dio = Dio();
   final TextEditingController _searchController = TextEditingController();
+  bool isLoading = false;
 
   List<dynamic> recipes = [];
   String mealType = "breakfast"; // Default meal type
@@ -24,7 +25,7 @@ class _MenuPageState extends State<MenuPage> {
   void initState() {
     super.initState();
     determineMealType(); // Determine the meal type based on the time of day
-    // fetchRecipes(""); // Fetch random recipes for the determined meal type
+    fetchRecipes(""); // Fetch random recipes for the determined meal type
   }
 
   void determineMealType() {
@@ -39,6 +40,10 @@ class _MenuPageState extends State<MenuPage> {
   }
 
   Future<void> fetchRecipes(String query) async {
+    setState(() {
+      isLoading = true;
+    });
+
     try {
       if (query.isNotEmpty) {
         // Perform search and get recipe IDs
@@ -51,17 +56,14 @@ class _MenuPageState extends State<MenuPage> {
         List<dynamic> detailedRecipes = [];
         for (var recipe in searchResults) {
           int id = recipe['id'];
-          try {
-            final detailResponse = await dio.get(
-              "https://api.spoonacular.com/recipes/$id/information?apiKey=$apiKey&includeNutrition=false",
-            );
-            detailedRecipes.add(detailResponse.data);
-          } catch (e) {
-            print("Error fetching details for recipe $id: $e");
-          }
+          final detailResponse = await dio.get(
+            "https://api.spoonacular.com/recipes/$id/information?apiKey=$apiKey&includeNutrition=false",
+          );
+          detailedRecipes.add(detailResponse.data);
         }
 
         setState(() {
+          isLoading = false;
           recipes = detailedRecipes;
         });
       } else {
@@ -70,6 +72,7 @@ class _MenuPageState extends State<MenuPage> {
           "https://api.spoonacular.com/recipes/random?apiKey=$apiKey&number=10&tags=$mealType&maxReadyTime=15&minServings=1&maxServings=2",
         );
         setState(() {
+          isLoading = false;
           recipes = response.data["recipes"];
         });
       }
@@ -137,151 +140,156 @@ class _MenuPageState extends State<MenuPage> {
           ),
           const SizedBox(height: 10),
           Expanded(
-            child: recipes.isEmpty
-                ? const Center(child: Text("No recipes found"))
-                : ListView.builder(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                    itemCount: recipes.length,
-                    itemBuilder: (context, index) {
-                      final recipe = recipes[index];
-                      final name = recipe['title'];
-                      final imageUrl = recipe["image"];
-                      final ingredients = recipe['extendedIngredients']
-                              ?.map<Widget>(
-                                (ingredient) => Text(
-                                  "-  ${ingredient['original']}",
-                                  style: const TextStyle(
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              )
-                              ?.toList() ??
-                          [
-                            const Text(
-                              "No ingredients available",
-                            )
+            child: isLoading
+                ? const Center(child: CupertinoActivityIndicator())
+                : recipes.isEmpty
+                    ? const Center(child: Text("No recipes found"))
+                    : ListView.builder(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        itemCount: recipes.length,
+                        itemBuilder: (context, index) {
+                          final recipe = recipes[index];
+                          final name = recipe['title'];
+                          final imageUrl = recipe["image"];
+                          final ingredients = recipe['extendedIngredients']
+                                  ?.map<Widget>(
+                                    (ingredient) => Text(
+                                      "-  ${ingredient['original']}",
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  )
+                                  ?.toList() ??
+                              [
+                                const Text(
+                                  "No ingredients available",
+                                )
+                              ];
+                          final List<Color> cardColors = [
+                            HexColor("#E4C1C1"), // Light orange
+                            HexColor("#97A78D"), // Light green
+                            HexColor("#ADB2D4"), // Light blue
                           ];
-                      final List<Color> cardColors = [
-                        HexColor("#E4C1C1"), // Light orange
-                        HexColor("#97A78D"), // Light green
-                        HexColor("#ADB2D4"), // Light blue
-                      ];
-                      final backgroundColor =
-                          cardColors[index % cardColors.length];
+                          final backgroundColor =
+                              cardColors[index % cardColors.length];
 
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                  builder: (context) =>
-                                      RecipeDetailPage(recipe: recipe)));
-                        },
-                        child: Card(
-                          color: backgroundColor,
-                          margin: const EdgeInsets.only(bottom: 30),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 16.0,
-                                right: 16.0,
-                                bottom: 16.0,
-                                top: 8.0),
-                            child: Column(
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) =>
+                                          RecipeDetailPage(recipe: recipe)));
+                            },
+                            child: Card(
+                              color: backgroundColor,
+                              margin: const EdgeInsets.only(bottom: 30),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 16.0,
+                                    right: 16.0,
+                                    bottom: 16.0,
+                                    top: 8.0),
+                                child: Column(
                                   children: [
-                                    Expanded(
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 8.0),
-                                        child: Text(
-                                          name,
-                                          style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                          maxLines: 3,
-                                          overflow: TextOverflow
-                                              .ellipsis, // Handle long names
-                                        ),
-                                      ),
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.arrow_forward_ios,
-                                        size: 16,
-                                      ),
-                                      onPressed: () {},
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    // Recipe Image
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(20),
-                                      child: Image.network(
-                                        imageUrl,
-                                        height: 128,
-                                        width: 128,
-                                        fit: BoxFit.cover,
-                                        errorBuilder:
-                                            (context, error, stackTrace) {
-                                          return Container(
-                                            height: 100,
-                                            width: 100,
-                                            color: Colors.grey,
-                                            child: const Center(
-                                              child: Text(
-                                                "Image not available",
-                                                style: TextStyle(
-                                                    color: Colors.white),
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 8.0),
+                                            child: Text(
+                                              name,
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
                                               ),
-                                            ),
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                        width:
-                                            16), // Spacing between image and text
-                                    // Recipe Details
-                                    Expanded(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          // Ingredients
-                                          Text(
-                                            "Ingredients:",
-                                            style: const TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold,
+                                              maxLines: 3,
+                                              overflow: TextOverflow
+                                                  .ellipsis, // Handle long names
                                             ),
                                           ),
-                                          const SizedBox(height: 4),
-                                          Column(
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(
+                                            Icons.arrow_forward_ios,
+                                            size: 16,
+                                          ),
+                                          onPressed: () {},
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        // Recipe Image
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          child: Image.network(
+                                            imageUrl,
+                                            height: 128,
+                                            width: 128,
+                                            fit: BoxFit.cover,
+                                            errorBuilder:
+                                                (context, error, stackTrace) {
+                                              return Container(
+                                                height: 100,
+                                                width: 100,
+                                                color: Colors.grey,
+                                                child: const Center(
+                                                  child: Text(
+                                                    "Image not available",
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                            width:
+                                                16), // Spacing between image and text
+                                        // Recipe Details
+                                        Expanded(
+                                          child: Column(
                                             crossAxisAlignment:
                                                 CrossAxisAlignment.start,
-                                            children: ingredients,
+                                            children: [
+                                              // Ingredients
+                                              Text(
+                                                "Ingredients:",
+                                                style: const TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 4),
+                                              Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: ingredients,
+                                              ),
+                                            ],
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
-                              ],
+                              ),
                             ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                          );
+                        },
+                      ),
           ),
         ],
       ),
